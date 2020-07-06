6049a337c450f264d54f6614d8cbd214dacb2e0f
Dmitry <it.portretnyy@gmail.com>
2020-07-06 22:21:38 +0300

    The difference between singleton and prototype scopes.

--- business-logic/src/main/java/ru/portretnyy/Application.java
+++ business-logic/src/main/java/ru/portretnyy/Application.java
@@ -7,6 +7,8 @@ public class Application {
     public static void main(String[] args) {
         final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         final Robot robot = context.getBean("robot", Robot.class);
+        final Robot anotherRobot = context.getBean("robot", Robot.class);
+        assert (robot == anotherRobot);
 
         robot.getCentralComputer().analyze();
         System.out.println("robot.getEngine().maxSpeed() = " + robot.getEngine().maxSpeed());
